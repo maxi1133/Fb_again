2460912ab417a59176419bf894da02933f9a1aa4
maxi1133 <sandop1202@gmail.com>
2020-05-18 22:09:07 +0700

    COnnect DB and create modal

--- be/server.js
+++ be/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
+
+
+
 
 ////////////// middle ware
 const bodyparser = require("body-parser");
@@ -10,17 +12,20 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 app.use(cors());
 
-////monggoose
-mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(
-  () => { console.log('Database is connected') },
-  err => { console.log('Can not connect to the database' + err) }
-);
+
+
+//////////////// Connect DB
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://maxi1133:123456789z@cluster0-jskdr.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
 
 ////////////// ROUTE
 app.use("/", (req, res) => {
   res.send('Welcome !')
 });
+
+
 
 
 /////////////////   SERVER
@@ -30,6 +35,11 @@ var server = http.createServer(app);
 server.listen(port, () => {
   console.log("server listen on port :" + port);
 });
+
+
+
+
+
 
 
 /////////////////   SOCKET IO
